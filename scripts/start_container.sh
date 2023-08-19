dbd18dfc2b2a469ee6a0acfdd585dc3a078e3e6c
sajal dhiman <92822169+Dhiman23@users.noreply.github.com>
2023-08-19 16:58:33 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull sajaldhimanitc1999/simple-python-flask-app
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sajaldhimanitc1999/simple-python-flask-app 
+docker run -d -p 5000:5000 sajaldhimanitc1999/simple-python-flask-app  
  
